46678856a98cc0eacfc00077011d3454b2399c42
Kai <mikeyu@KaideMacBook-Pro.local>
2022-08-22 08:57:54 +0800

    searchCityFunction

--- Weather/WeatherIndexController.h
+++ Weather/WeatherIndexController.h
@@ -11,7 +11,6 @@
 NS_ASSUME_NONNULL_BEGIN
 
 @interface WeatherIndexController : UIViewController
-@property (nonatomic, weak) IBOutlet UITableViewCell *IndexTV;
 @property (atomic, weak) WeatherData *weatherData;
 @end
 
